e4dcf5f45bf53ad387c75c4b4b10ae5a59c94cc6
Jake Kaupp <jkaupp@gmail.com>
2018-01-04 20:10:39 +0000

    finished cleaning

--- January/R/cleaning.R
+++ January/R/cleaning.R
@@ -6,8 +6,25 @@ library(rvest)
 url <- "http://aquatext.com/tables/algaegrwth.htm"
 
 # Discard the header and the footer rows
+# Remove line returns
 algae_tbl <- read_html(url) %>% 
   html_table(fill = TRUE) %>% 
   discard(~nrow(.x) <= 6) %>% 
-  flatten_df()
+  flatten_df() %>% 
+  mutate_all(funs(gsub("\n","", .))) %>% 
+  mutate_all(funs(gsub("\\s+", " ", .)))
 
+
+tbl_headers <- algae_tbl %>% 
+  slice(1:2) %>% 
+  select(-1) %>% 
+  summarize_all(funs(paste0(., collapse= "_"))) %>% 
+  flatten_chr() %>% 
+  c("species",.)
+
+tidy_algae <- algae_tbl %>% 
+  set_names(tbl_headers) %>% 
+  slice(-1:-3) %>% 
+  gather(var, divisions, -species) %>% 
+  separate(var,c("temperature", "light_intensity"), sep = "_") %>% 
+  arrange(species)
