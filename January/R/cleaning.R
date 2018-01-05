eadd9e5a651a5b1ec7e457f9d03024b423d51386
Jake Kaupp <jkaupp@gmail.com>
2018-01-05 02:26:47 +0000

    added ridgline plots

--- January/R/cleaning.R
+++ January/R/cleaning.R
@@ -1,18 +1,21 @@
 library(tidyverse)
 library(rvest)
+library(ggridges)
+library(jkmisc)
 
 
 # Read in data
 url <- "http://aquatext.com/tables/algaegrwth.htm"
 
 # Discard the header and the footer rows
-# Remove line returns
+# Remove line returns and convert extra spaces into single spaces.
 algae_tbl <- read_html(url) %>% 
   html_table(fill = TRUE) %>% 
   discard(~nrow(.x) <= 6) %>% 
   flatten_df() %>% 
   mutate_all(funs(gsub("\n","", .))) %>% 
-  mutate_all(funs(gsub("\\s+", " ", .)))
+  mutate_all(funs(gsub("\\s+", " ", .))) %>% 
+  mutate_all(funs(gsub("\\.+", ".", .)))
 
 
 tbl_headers <- algae_tbl %>% 
@@ -27,4 +30,14 @@ tidy_algae <- algae_tbl %>%
   slice(-1:-3) %>% 
   gather(var, divisions, -species) %>% 
   separate(var,c("temperature", "light_intensity"), sep = "_") %>% 
-  arrange(species)
+  mutate_at(c("temperature","light_intensity", "divisions"), as.numeric) %>% 
+  group_by(species, temperature) %>% 
+  mutate(color = ifelse(divisions < 0, 1, 0)) %>% 
+  arrange(species, temperature, light_intensity) 
+
+
+ggplot(tidy_algae, aes(x = temperature, y = species, group = species)) +
+  geom_ridgeline(aes(height = divisions, fill = light_intensity), min_height = -0.8, scale = 0.6, size = 0.2, color = "grey20") +
+  facet_wrap(~light_intensity, nrow = 1) +
+  scale_x_continuous(breaks = seq(5, 30, 5)) +
+  theme_jk()
